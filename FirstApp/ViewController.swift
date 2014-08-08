64f810e40ee20bf070e811b03c24d1f27f2a607f
Adrian Gherle <adriangherle@adrians-mbp.home>
2014-08-07 19:02:40 -0700

    initial commit

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+//    @IBOutlet weak var clickMe: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func displayMsg(sender: UIButton)
+    {
+        println("Button was clicked!")
+    }
+    
+    
+    @IBAction func displaySwitchMsg(sender: UISwitch)
+    {
+        if sender.on {
+            println("Switch is on!")
+        } else {
+            println("Switch is off")
+        }
+        
+    }
 
+    @IBAction func displaySliderValue(sender: UISlider)
+        
+    {
+        if sender.value > 0.5 {
+            println("Slider value is: \(sender.value)")
+        }
+    }
 
 }
 
